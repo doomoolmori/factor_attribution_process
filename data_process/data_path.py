b798aef57c773eacb64054e4759c4f8f60a31604
Jamal <byungju.kim@doomoolmori.com>
2022-08-03 17:51:38 +0900

    init

---
+++ data_process/data_path.py
@@ -0,0 +1,17 @@
+import os
+
+FACTOR_CATEGORY_PATH = f'{os.getcwd()}/'
+FACTOR_CATEGORY_NAME = 'QT_factor_category_compress_final.csv'
+
+RAW_DATA_PATH = f'{os.getcwd()}/'
+RAW_DATA_NAME = '2022-05-27_cosmos-univ-with-factors_with-finval_global_monthly.csv'
+
+DICT_OF_KOREA_DATA_PATH = f'{os.getcwd()}/data/korea'
+DICT_OF_KOREA_DATA_NAME = 'korea_dict_of_data.pickle'
+
+DICT_OF_US_DATA_PATH = f'{os.getcwd()}/data/us'
+DICT_OF_US_DATA_NAME = 'us_dict_of_data.pickle'
+
+KOREA_UNIVERSE = 'Univ_KOSPI&KOSDAQ'
+US_UNIVERSE = 'Univ_S&P500'
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import polars as pl
+import os
+
+from data_process import data_read
